09d1cd90df8c856513bc7f074aa6706e18dced42
Giulianogoliver <giulianogoliver84@gmail.com>
2023-12-11 20:44:36 -0300

    exploring Boolean type

---
+++ JavaScript/primitive types/Boolean/index.js
@@ -0,0 +1,12 @@
+//  Tipo Booleano
+
+//  Apenas dois retornos
+console.log(true);
+console.log(false);
+
+//  A comparação retorna valor booleando
+console.log(1 === 2);
+console.log(1 === 1);
+
+// Verificando o tipo
+console.log(typeof false);

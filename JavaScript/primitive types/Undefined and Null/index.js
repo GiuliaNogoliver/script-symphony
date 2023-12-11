65927101f695e53228d7d2c34f5ee32dfe456d71
Giulianogoliver <giulianogoliver84@gmail.com>
2023-12-11 20:54:37 -0300

    exploring Undefined and null

---
+++ JavaScript/primitive types/Undefined and Null/index.js
@@ -0,0 +1,16 @@
+//  Tipo Undefined
+
+/*let anwser;
+console.log(answer);*/
+//Ele da erro pois nao tem nada definido
+
+//Se vc declarar dentro do console ele retorna exatamente isso
+console.log(undefined);
+console.log(null);
+
+// Note que VAZIO é diferente de null
+console.log("");
+console.log(null == "");
+
+// Note também que ambos São diferentes
+console.log(undefined == null);

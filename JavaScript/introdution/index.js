8949967283ee9f8893520d678a6e010a8ac75e5c
Giulianogoliver <giulianogoliver84@gmail.com>
2023-12-11 20:40:36 -0300

    exploring Number type

--- JavaScript/introdution/index.js
+++ JavaScript/introdution/index.js
@@ -1,1 +1,3 @@
 console.log("Olá mundo!");
+
+//ctrl alt n -> roda o codigo atual
---
+++ JavaScript/primitive types/Number/index.js
@@ -0,0 +1,16 @@
+//Tipo Number
+
+console.log(123);
+console.log("123");
+
+console.log(123 + 1);
+console.log(123 - 1);
+console.log("123" + 1); //concatena 
+console.log("123" - 1);
+
+// Converter em número sempre que for fazer operação
+console.log(Number("123") + 1);
+
+// Verifica o tipo do dado/variavel
+console.log(typeof Number("123"));
+console.log(typeof "123");

3e39c6cdf198943e4c222a0e21bc771215bda0aa
Giulianogoliver <giulianogoliver84@gmail.com>
2023-12-12 11:15:28 -0300

    exploring addiction

---
+++ JavaScript/arithmetic operators/addition/index.js
@@ -0,0 +1,10 @@
+// Adição e concatenação
+
+adicao = 1 + 2;
+console.log(adicao);
+
+concatenacao = "Giulia" + "Nogueira";
+console.log(concatenacao);
+
+adicaoEConcatenacao = "Giuia" + " tem " + (20 + 1) + " anos";
+console.log(adicaoEConcatenacao);

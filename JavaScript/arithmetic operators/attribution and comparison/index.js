912171ba8896c3487b4f66702f4b030a3bd53bcf
Giulianogoliver <giulianogoliver84@gmail.com>
2023-12-12 12:49:54 -0300

    exploring attribuition

---
+++ JavaScript/arithmetic operators/attribution and comparison/index.js
@@ -0,0 +1,28 @@
+// Atribuição
+
+//  =, +=, -=, *=, /=, %=
+//  let e var -> trabalha por excopo, pode ser alterado
+//  const -> é constante, nao pode ser alterado
+
+let a = 6;
+console.log(a);
+
+let b = 2;
+console.log(b);
+
+a += b;
+console.log(a);
+
+a -= b;
+console.log(a);
+
+a *= b;
+console.log(a);
+
+a /= b;
+console.log(a);
+
+a %= b;
+console.log(a);
+
+//  o valor esta sendo alterado e manipulado linha por linha, naoi reseta o valor da variavel antes de cada operacao

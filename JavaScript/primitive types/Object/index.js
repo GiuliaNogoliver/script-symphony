835afb1f9f1cf74a5813af0867300fb377645c1a
Giulianogoliver <giulianogoliver84@gmail.com>
2023-12-11 21:34:51 -0300

    exploring Object type

---
+++ JavaScript/primitive types/Object/index.js
@@ -0,0 +1,34 @@
+// Tipo Objeto
+
+//  Para declarar se coloca entre {chaves}
+objeto01 = {};
+console.log(objeto01);
+
+//  assim como em java ele pode ter atributos e metodos
+objeto02 = {
+    cor: String,
+    tamanho: Number,
+    peso: Number
+}
+//  se declara o nome da propriedade dois pontos: e tipo de valor q recebe
+console.log(objeto02);
+
+
+//  Pode se também já declarar o valor dos atributos do objeto
+objeto03 = {
+    cor: "rosa",
+    tamanho: 34,
+    peso: 200
+};
+console.log(objeto03.cor, objeto03.tamanho, objeto03.peso);
+
+// declarando um objeto dentro de outro objeto
+objeto04 = {
+    cor: String,
+    objeto05 : {
+        tipo: String,
+        formato: String,
+        grande: false
+    }
+};
+console.log(objeto04 ,objeto04.objeto05);

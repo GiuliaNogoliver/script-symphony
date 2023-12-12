2b277c44d2bdea18324098d060841d5c29a7ecd1
Giulianogoliver <giulianogoliver84@gmail.com>
2023-12-12 12:25:53 -0300

    exploring multiplication and division

---
+++ JavaScript/arithmetic operators/addition and subtraction/index.js
@@ -0,0 +1,24 @@
+// Adição e concatenação
+
+adicao = 1 + 2;
+console.log(adicao);
+
+concatenacao = "Giulia" + "Nogueira";
+console.log(concatenacao);
+
+adicaoEConcatenacao = "Giuia" + " tem " + (20 + 1) + " anos";
+console.log(adicaoEConcatenacao);
+
+
+
+// Subtração
+
+subtracao = 2 - 1;
+console.log(subtracao);
+
+// quando se subtrai ele converte a string mesmo sem converter
+console.log("2" - 1);
+console.log(2 - "3");
+
+// mesmo assim o ideal é sempre converter
+console.log(Number("6") - 3);
---
+++ JavaScript/arithmetic operators/multiplication and division/index.js
@@ -0,0 +1,15 @@
+//  Multiplicação
+
+multiplicacao = 10 * 5;
+console.log(multiplicacao);
+
+multiplicacao01 = "10" * 5;
+console.log(multiplicacao);
+
+// Divisão
+
+divisao = 10 / 5;
+console.log(divisao);
+
+divisao01 = "10" / 5;
+console.log(divisao01);

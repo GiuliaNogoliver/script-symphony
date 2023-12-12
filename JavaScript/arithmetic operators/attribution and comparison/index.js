49f9e1589552cd53c0ce411c6a84b8a4518d452c
Giulianogoliver <giulianogoliver84@gmail.com>
2023-12-12 14:47:41 -0300

    exploring comparison

--- JavaScript/arithmetic operators/attribution and comparison/index.js
+++ JavaScript/arithmetic operators/attribution and comparison/index.js
@@ -1,8 +1,9 @@
 // Atribuição
 
-//  =, +=, -=, *=, /=, %=
-//  let e var -> trabalha por excopo, pode ser alterado
-//  const -> é constante, nao pode ser alterado
+/*  =, +=, -=, *=, /=, %=
+ *  let e var -> trabalha por excopo, pode ser alterado
+ *  const -> é constante, nao pode ser alterado
+ */
 
 let a = 6;
 console.log(a);
@@ -12,6 +13,8 @@ console.log(b);
 
 a += b;
 console.log(a);
+
+//  o valor esta sendo alterado e manipulado linha por linha, naoi reseta o valor da variavel antes de cada operacao
 
 a -= b;
 console.log(a);
@@ -25,4 +28,27 @@ console.log(a);
 a %= b;
 console.log(a);
 
-//  o valor esta sendo alterado e manipulado linha por linha, naoi reseta o valor da variavel antes de cada operacao
+
+//  Comparação
+
+/*
+*   operadores - retornam boolean
+*   == compara somente o valor da variavel
+*   === compara tipo e o valor da variavel
+*   != compara somente o valor da variavel
+*   !== compara tipo e o valor da variavel
+*   >, <, <=, >=
+*/
+
+a = 5;
+b ="5";
+console.log(a == b);
+console.log(a === b);
+
+console.log(a != b);
+console.log(a !== b);
+
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);

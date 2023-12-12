9faaa1ea840052eb466615f720a283fa429f6d9c
Giulianogoliver <giulianogoliver84@gmail.com>
2023-12-12 11:22:00 -0300

    exploring subtraction

---
+++ JavaScript/arithmetic operators/subtraction/index.js
@@ -0,0 +1,11 @@
+// Subtração
+
+subtracao = 2 - 1;
+console.log(subtracao);
+
+// quando se subtrai ele converte a string mesmo sem converter
+console.log("2" - 1);
+console.log(2 - "3");
+
+// mesmo assim o ideal é sempre converter
+console.log(Number("6") - 3);

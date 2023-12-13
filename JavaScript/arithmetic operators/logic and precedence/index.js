0c48d70a50c6a81485cee860306a1bb5b3523e87
Giulianogoliver <giulianogoliver84@gmail.com>
2023-12-13 20:00:31 -0300

    Exploring logic operations

---
+++ JavaScript/arithmetic operators/logic and precedence/index.js
@@ -0,0 +1,16 @@
+// Operador Lógico
+
+/* && (E)
+ * || (Ou)
+ *  ! (Negaçao)
+ */
+
+a = 5;
+b = 6;
+c = "5";
+
+console.log(a === c && a == c);
+console.log(a == b || a == c);
+console.log(a === c && a == c);
+console.log(!false);
+console.log(!a == c);

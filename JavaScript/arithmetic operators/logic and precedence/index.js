2a7b8bf496892c6e730b1905e5b2bebefb2a3d23
Giulianogoliver <giulianogoliver84@gmail.com>
2023-12-13 20:05:03 -0300

    Exploring precedence

--- JavaScript/arithmetic operators/logic and precedence/index.js
+++ JavaScript/arithmetic operators/logic and precedence/index.js
@@ -14,3 +14,15 @@ console.log(a == b || a == c);
 console.log(a === c && a == c);
 console.log(!false);
 console.log(!a == c);
+
+// Operador Precedencia
+
+// O javascript opera numa ordem ele nao seleciona oq opera apenas tudo q vier da direota para a esquerda
+// oq muda é colocar entre parenteses as operacaos q vc deseja fazer primerio
+
+a = false;
+b = true;
+
+(a && b) === false;
+
+(15 * 20) / 20 == 20;

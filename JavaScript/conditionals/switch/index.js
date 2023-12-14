c007918721efa102dee5662d89219d93539adc21
Giulianogoliver <giulianogoliver84@gmail.com>
2023-12-14 20:28:08 -0300

    exploring switch

---
+++ JavaScript/conditionals/switch/index.js
@@ -0,0 +1,16 @@
+// Condicional Switch
+
+const mes = 5;
+
+switch (mes) {
+    case 1: console.log("Janiero");
+        break;
+    case 2: console.log("Fevereiro");
+        break;
+    case 3: console.log("Março");
+        break;
+    case 4: console.log("Abril");
+        break;
+    case 5: console.log("Maio");
+        break;                          // break é encontrei fiz fechei
+}

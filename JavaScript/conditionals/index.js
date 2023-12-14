8697ff3167ef707020d47f141db61ebdc2e6ac25
Giulianogoliver <giulianogoliver84@gmail.com>
2023-12-14 19:48:12 -0300

    exploring If Else

---
+++ JavaScript/conditionals/index.js
@@ -0,0 +1,19 @@
+// Condicional If Else
+
+let variavel = 4;
+
+if (variavel <= 0){
+    console.log("A variavél é nula ou negativa");
+} else {
+    console.log("A variavél nao é nula ou negativa");
+}
+
+let variavelDois = 1;
+
+if (variavelDois == variavel){
+    console.log("As variaveis sao iguais");
+} else if (variavelDois > variavel){
+    console.log("A variavel dois é mais que a primeira");
+} else {
+    console.log("As variaveis sao diferentes");
+}

6aaa38bff0c19fd0c2a1409c9d42e66865aebf85
Giulianogoliver <giulianogoliver84@gmail.com>
2023-12-14 20:20:11 -0300

    exploring ternary

---
+++ JavaScript/conditionals/ternary/index.js
@@ -0,0 +1,11 @@
+// Operador Ternário
+
+// ele se baseia em possuir uma condiçao, caso seja verdadeiro (?) ent colocar o retorno caso seja falso (:) retorno
+const speed = 75;
+const warn = 50;
+
+const condition = speed > warn 
+    ? console.log("Levou uma multa!") 
+    : console.log("Esta no limite de velocidade");
+
+// Um tipo diferente de If e Else

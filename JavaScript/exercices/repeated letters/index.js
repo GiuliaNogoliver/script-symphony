0edad1f04a5b2b31b175584b30672ee7ec455070
Giulianogoliver <giulianogoliver84@gmail.com>
2023-12-17 21:35:47 -0300

    challange with for each

---
+++ JavaScript/exercices/repeated letters/index.js
@@ -0,0 +1,16 @@
+// Fazer um codigo que diz quando letras repetidas aparecem em uma palavra
+
+// exemplo Abacate = {a: 3, b: 1, c: 1, t: 1, e: 1}
+
+const word = "Wellington e Giulia".toLowerCase().replaceAll(" ", "");
+let letters = {};
+
+for(i = 0;i < palavra.length; i++){ // laço normal
+    if(letters[word[i]]){         // aqui se existir a letra, lembra do truth
+        letters[word[i]]++;       // ent acrescenta a letra
+    } else {
+        letters[word[i]] = 1;     // Se nao apenas 1
+    }
+}
+
+console.log(letters);

f822d71eede580ebb1260ad7a2772e700c8ca413
Giulianogoliver <giulianogoliver84@gmail.com>
2024-01-19 16:49:13 -0300

    learning how to access the values of objects

---
+++ JavaScript/08 - deeping objects/8.3 - object's values access/index.js
@@ -0,0 +1,9 @@
+const item = {
+    tipo: "Teclado",
+    valor: 'R$190',
+    estoque: true
+}
+
+console.log(item.tipo);
+console.log("O produto " + `${item.tipo}` + " está saindo por " + `${item.valor}`);
+console.log(`O produto ${item.tipo} está saindo por ${item.valor}`);

1cb3763e435b5ec4e6047594e3b4671ae9d7cd94
Giulianogoliver <giulianogoliver84@gmail.com>
2024-01-25 12:52:13 -0300

    learning how to remove elements in an array

---
+++ JavaScript/09 - deeping array/8.6 - remove elements/index.js
@@ -0,0 +1,11 @@
+let arr = ['a', 1, 2, 3, 4, 5, 'b'];
+
+console.table(arr);
+
+// O shift remove o primeiro element e retorna ele
+console.table(arr.shift(arr));
+console.table(arr);
+
+// O pop remove o ultimo elemento de um array e retorna ele
+console.table(arr.pop(arr));
+console.table(arr);

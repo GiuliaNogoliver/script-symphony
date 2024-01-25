fc40aef5231505d50d3e9549552877136d3e9792
Giulianogoliver <giulianogoliver84@gmail.com>
2024-01-25 12:48:05 -0300

    learning how to add elements in an array

---
+++ JavaScript/09 - deeping array/8.5 - add elements/index.js
@@ -0,0 +1,18 @@
+let arr = [1, 2,, 3, 4, 5];
+
+// adicionando por index
+arr[5] = 6;
+
+console.table(arr);
+
+// adiciona no fim do array
+arr.push(7);
+
+console.table(arr);
+
+// adiciona no começo do array
+arr.unshift(0);
+arr.unshift(-1);
+
+console.table(arr);
+

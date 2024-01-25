aafce5ed5c3c88e243b485c5bba6625e3b638cae
Giulianogoliver <giulianogoliver84@gmail.com>
2024-01-25 12:37:32 -0300

    spread operator vulgo ...

---
+++ JavaScript/09 - deeping array/8.4 - spread operator/index.js
@@ -0,0 +1,11 @@
+const num = [1, 2, 3, 4, 5];
+
+// os tres pontinhos tira a formatacao de array
+console.log(...num);
+
+
+// funciinalidade que verifica o maior numero
+console.log(Math.max(...num));
+
+// assim n funciona
+console.log(Math.max(num));

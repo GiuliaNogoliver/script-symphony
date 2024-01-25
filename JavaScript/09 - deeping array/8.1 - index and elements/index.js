351223d8f84ecfbb711726218d643d4b9e9923d1
Giulianogoliver <giulianogoliver84@gmail.com>
2024-01-25 11:31:47 -0300

    understandinh better index and elements

---
+++ JavaScript/09 - deeping array/8.1 - index and elements/index.js
@@ -0,0 +1,7 @@
+const arr = [1, 2, 3, 4, true, 'Giulia', {nome: 'Giulia Nogueira'}];
+// esse table retorna os indexs e os elementos
+console.table(arr);
+
+const arr2 = [1, 2, 3, 4, true, 'Giulia','Nogueira'];
+console.table(arr2);
+console.log(arr2[6]);

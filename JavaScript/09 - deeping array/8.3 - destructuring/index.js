a7712ee72c645aee849b7784753beccb0b441a8a
Giulianogoliver <giulianogoliver84@gmail.com>
2024-01-25 12:32:47 -0300

    desestructing arrays

---
+++ JavaScript/09 - deeping array/8.3 - destructuring/index.js
@@ -0,0 +1,9 @@
+const [a, b, c] = ["Arroz", "Feijão", "Alface"];
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+const comida = ["Arroz", "Feijão", "Alface"];
+
+console.log(comida[0], comida[1], comida[2]);

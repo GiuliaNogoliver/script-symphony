7b3fd25cdfdcf06de563b7810d15f32a03d148e9
Giulianogoliver <giulianogoliver84@gmail.com>
2024-01-29 09:47:32 -0300

    reverse order for numbers and objects

--- JavaScript/09 - deeping array/8.8 - order and reverse values/index.js
+++ JavaScript/09 - deeping array/8.8 - order and reverse values/index.js
@@ -2,9 +2,11 @@ const arrayNumeros = [32, 48, 93, 32634, 2332, 2, 4, 838];
 
 // Ordena a ordem crescente se for b - a fica decrescente
 console.log(arrayNumeros.sort((a, b) => a - b));
+console.log(arrayNumeros.sort((a, b) => b - a));
 
 
 const arrayPalavras = [{nome: 'Giulia'}, {nome: 'Wellington'}, {nome: 'Giovanna'}, {nome: 'Victor'}];
 
 // para ordenar palavras por letra
 console.log(arrayPalavras.sort((a, b) => a.nome.localeCompare(b.nome)));
+console.log(arrayPalavras.sort((a, b) => a.nome.localeCompare(b.nome)).reverse());

79acf190ea46c908d4a489b4c41efc0efc7057f4
Giulianogoliver <giulianogoliver84@gmail.com>
2024-01-30 19:53:06 -0300

    Map in array

---
+++ JavaScript/09 - deeping array/8.12 - map/index.js
@@ -0,0 +1,32 @@
+const pedidos = [
+    {
+        id: 420,
+        nome: "Giulia",
+        alimento: "Sanduiche Vegano",
+        bebida: "Suco de Uva"
+    },
+    {
+        id: 123,
+        nome: "Wellington",
+        alimento: "Sandubao de Bacon",
+        bebida: "Agua"
+    },
+    {
+        id: 421,
+        nome: "Giovanna",
+        alimento: "Porçao de coxinha",
+        bebida: "Coca cola"
+    }
+];
+
+// Map
+
+pedidos.map((element, index) => {
+    console.log(index, element);
+    if(element.id == 420){              // o == compara
+        element.alimento = "Porcao de Batata frita"; // o = atribui
+        console.log(index, element);
+    };
+});
+
+// O map serve para iterar os elementos do array

5d9c8bf00e34e1fe5d9d811256e60327f14bb003
Giulianogoliver <giulianogoliver84@gmail.com>
2024-01-31 19:04:26 -0300

    what's is it

--- JavaScript/09 - deeping array/8.13 - filter/index.js
+++ JavaScript/09 - deeping array/8.13 - filter/index.js
@@ -1,39 +1,40 @@
 const pedidos = [
-    {
-        id: 420,
-        nome: "Giulia",
-        alimento: "Sanduiche Vegano",
-        bebida: "Suco de uva"
-    },
-    {
-        id: 123,
-        nome: "Wellington",
-        alimento: "Sandubao de Bacon",
-        bebida: "Agua"
-    },
-    {
-        id: 421,
-        nome: "Giovanna",
-        alimento: "Porçao de coxinha",
-        bebida: "Coca cola"
-    },
-    {
-        id: 134,
-        nome: "Arnaldo",
-        alimento: "Sandubao de Carne",
-        bebida: "Suco de laranja"
-    },
-    {
-        id: 92,
-        nome: "Denner",
-        alimento: "Porcao de batata frita",
-        bebida: "Coca cola"
-    }
+  {
+    id: 420,
+    nome: "Giulia",
+    alimento: "Sanduiche Vegano",
+    bebida: "Suco de uva",
+  },
+  {
+    id: 123,
+    nome: "Wellington",
+    alimento: "Sandubao de Bacon",
+    bebida: "Agua",
+  },
+  {
+    id: 421,
+    nome: "Giovanna",
+    alimento: "Porçao de coxinha",
+    bebida: "Coca cola",
+  },
+  {
+    id: 134,
+    nome: "Arnaldo",
+    alimento: "Sandubao de Carne",
+    bebida: "Suco de laranja",
+  },
+  {
+    id: 92,
+    nome: "Denner",
+    alimento: "Porcao de batata frita",
+    bebida: "Coca cola",
+  },
 ];
 
 // O filter faz exatamente o trabalho de filtrar de acordo com a condicao que vc colocar
 
-const bebidasFilter = pedidos.filter((element, index) => {  // armazenando os dados filtrados numa nova const
-    return element.bebida == "Coca cola";
+const bebidasFilter = pedidos.filter((element, index) => {
+  // armazenando os dados filtrados numa nova const
+  return element.bebida == "Coca cola";
 });
 console.log(bebidasFilter);
---
+++ JavaScript/10 - DOM/10.0 - what's it/script.js
@@ -0,0 +1,5 @@
+"use-strict";
+
+const h1 = document.querySelector("h1");
+// const h1 = document.querySelector("p");
+h1.style.background = "Red";
